38e43413d3b782206a6a8fb4d2f03e598912a7e2
ghostbaby4 <vanegajudith26@gmail.com>
2026-05-24 14:40:18 -0600

    Conexión backend y google analytics

--- scripts.js
+++ scripts.js
@@ -1,5 +1,97 @@
 console.log("Script cargado correctamente");
 
+///// Inicio conexión API formulario
+const form = document.getElementById('contactForm');
+
+if (form) {
+
+  form.addEventListener('submit', async (e) => {
+
+    e.preventDefault();
+
+    const data = {
+
+      nombre: document.getElementById('nombre').value,
+
+      apellido: document.getElementById('apellido').value,
+
+      telefono: document.getElementById('telefono').value,
+
+      correo: document.getElementById('correo').value,
+
+      tipo_cliente: document.getElementById('tipo_cliente').value,
+
+      departamento: document.getElementById('departamento').value,
+
+      servicio_interes: document.getElementById('servicio').value,
+
+      comentario: document.getElementById('comentario').value
+
+    };
+
+    console.log(data);
+
+    try {
+
+      const response = await fetch(
+        'http://127.0.0.1:8000/catalogo/contactos/',
+        {
+
+          method: 'POST',
+
+          headers: {
+            'Content-Type': 'application/json'
+          },
+
+          body: JSON.stringify(data)
+
+        }
+      );
+
+      console.log("STATUS:", response.status);
+
+      const result = await response.json();
+
+      console.log("RESPUESTA:", result);
+
+      if (response.ok) {
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Mensaje enviado',
+          text: 'Nos pondremos en contacto contigo pronto.',
+          confirmButtonColor: '#4a7fc1'
+        });
+
+        form.reset();
+
+      } else {
+
+        const errorData = await response.json();
+
+        console.log(errorData);
+
+        Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No se pudo enviar el formulario.',
+        confirmButtonColor: '#081b29'
+        });
+
+      }
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
+  });
+
+}
+///// Fin conexión API formulario
+
+
 //// Navbar Scroll
 window.addEventListener('scroll', function(){
   let Navbar = document.querySelector('.landing-header');
@@ -54,4 +146,5 @@ document.addEventListener('DOMContentLoaded', () => {
   animarAlScroll(seccionServicios, 'animate__fadeInLeftBig');
 });
 /////Fin de la animación scroll
+
 
